1d9ad8f9a333811d373e2cae4bf842e5f86a45a3
Jeffrey Su <zsu@senparc.com>
2019-02-02 22:30:34 +0800

    添加 EntityBase

---
+++ src/Senparc.Scf.Core/Models/EntityBase/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Scf.Core
+{
+    public class EntityBase
+    {
+    }
+}
---
+++ src/Senparc.Scf.Core/Models/EntityBase/IEntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Scf.Core.Models.EntityBase
+{
+    public class IEntityBase
+    {
+    }
+}
