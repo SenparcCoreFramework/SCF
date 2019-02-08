19ed4a3b367cfc2ae88fab106c8003ea108606b1
Jeffrey Su <zsu@senparc.com>
2019-02-08 23:05:16 +0800

    添加新的Razor Page - Areas.Admin，编译通过

---
+++ src/Senparc.Areas.Admin/BaseAdminPageModel.cs
@@ -0,0 +1,21 @@
+﻿using Senparc.Core.Models.VD;
+using Senparc.Scf.Core.Models;
+using Senparc.Scf.Core.Models.VD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senparc.Areas.Admin
+{
+
+    public interface IBaseAdminPageModel : IBasePageModel
+    {
+
+    }
+
+    public class BaseAdminPageModel : PageModelBase, IBaseAdminPageModel
+    {
+     
+    }
+}
