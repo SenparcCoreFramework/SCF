690b60708984bd62b42dac8304d1a9d62c4fc6ad
Jeffrey Su <zsu@senparc.com>
2019-02-09 14:36:24 +0800

    添加登录页面

---
+++ src/Senparc.Areas.Admin/Areas/Admin/Pages/Login.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Senparc.Areas.Admin.Areas.Admin.Pages
+{
+    public class LoginModel : BaseAdminPageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
