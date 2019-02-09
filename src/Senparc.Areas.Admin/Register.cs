177f0a5daa500717440d1411fcd2347e815ac67c
Jeffrey Su <zsu@senparc.com>
2019-02-09 17:37:37 +0800

    添加 AreaRegister

---
+++ src/Senparc.Areas.Admin/Register.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Senparc.Scf.Core.Areas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Areas.Admin
+{
+    public class Register : IAreaRegister
+    {
+        public IMvcBuilder AuthorizeConfig(IMvcBuilder builder)
+        {
+            builder.AddRazorPagesOptions(options => {
+                options.Conventions.AuthorizePage("/Admin");//必须登录
+                options.Conventions.AllowAnonymousToPage("/Admin/Login");//允许匿名
+                //更多：https://docs.microsoft.com/en-us/aspnet/core/security/authorization/razor-pages-authorization?view=aspnetcore-2.2
+            });
+
+            return builder;
+        }
+    }
+}
---
+++ src/Senparc.Scf.Core/Areas/AreaRegister.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Senparc.CO2NET.Cache;
+using Senparc.CO2NET.Trace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.Scf.Core.Areas
+{
+    /// <summary>
+    /// 对所有扩展 Area 进行注册
+    /// </summary>
+    public class AreaRegister
+    {
+        public bool RegisterAreasFinished { get; set; }
+
+        public IMvcBuilder RegisterAllAreas(IMvcBuilder builder)
+        {
+            //遍历所有程序集进行注册
+
+            var dt1 = SystemTime.Now;
+
+            var cacheStragegy = CacheStrategyFactory.GetObjectCacheStrategyInstance();
+            using (cacheStragegy.BeginCacheLock("Senparc.Scf.Core.Areas.AreaRegister", "Senparc.Scf.Core.Areas"))
+            {
+                if (RegisterAreasFinished == true)
+                {
+                    return builder;
+                }
+
+                //查找所有扩展缓存
+                var scanTypesCount = 0;
+
+                var assembiles = AppDomain.CurrentDomain.GetAssemblies();
+
+                foreach (var assembly in assembiles)
+                {
+                    try
+                    {
+                        scanTypesCount++;
+                        var areaRegisterTypes = assembly.GetTypes()
+                                    .Where(z => z.GetInterface("IAreaRegister") != null)
+                                    .ToArray();
+
+                        foreach (var registerType in areaRegisterTypes)
+                        {
+                            var register = Activator.CreateInstance(registerType, true) as IAreaRegister;
+                            register.AuthorizeConfig(builder);//进行注册
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        SenparcTrace.SendCustomLog("RegisterAllAreas() 自动扫描程序集报告（非程序异常）：" + assembly.FullName, ex.ToString());
+                    }
+                }
+
+                RegisterAreasFinished = true;
+
+                var dt2 = SystemTime.Now;
+                Console.WriteLine($"RegisterAllAreas 用时：{(dt2 - dt1).TotalMilliseconds}ms");
+
+                return builder;
+            }
+        }
+    }
+}
