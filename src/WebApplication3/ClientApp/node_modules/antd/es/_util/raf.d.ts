2bb34c72a5507c1af002608ab6d2a5871a6aa85e
Jeffrey Su <zsu@senparc.com>
2019-02-18 15:25:48 +0800

    清理测试文件

--- src/WebApplication3/ClientApp/node_modules/antd/es/_util/raf.d.ts
+++
@@ -1,4 +0,0 @@
-export default function wrapperRaf(callback: () => void, delayFrames?: number): number;
-export default namespace wrapperRaf {
-    var cancel: (id: number) => void;
-}
--- src/WebApplication3/ClientApp/node_modules/antd/lib/grid/RowContext.d.ts
+++
@@ -1,6 +0,0 @@
-import { Context } from 'create-react-context';
-export interface RowContextState {
-    gutter?: number;
-}
-declare const RowContext: Context<RowContextState>;
-export default RowContext;
