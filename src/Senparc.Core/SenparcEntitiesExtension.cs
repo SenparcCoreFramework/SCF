8d3b07b853131be536b3697108ede006b64f2b12
Jeffrey Su <zsu@senparc.com>
2019-06-03 18:11:11 +0800

    更新SDK引用

--- src/Senparc.Core/SenparcEntitiesExtension.cs
+++ src/Senparc.Core/SenparcEntitiesExtension.cs
@@ -25,7 +25,7 @@ namespace Senparc.Core
             //                .Options));
             //#endif
 
-            SenparcDI.GlobalIServiceProvider = null;//清空缓存，下次使用DI会自动重新Build
+            SenparcDI.ResetGlobalIServiceProvider();//清空缓存，下次使用DI会自动重新Build
 
             return services;
         }
