88e8e0201323dee6943d37b887abfb9c53c490e2
苏震巍 <zsu@senparc.com>
2019-08-24 16:23:50 +0800

    针对”EnableRetryOnFailure 解决短暂的数据库连接失败”问题的注释说明

--- src/Senparc.Core/SenparcEntitiesExtension.cs
+++ src/Senparc.Core/SenparcEntitiesExtension.cs
@@ -13,6 +13,14 @@ namespace Senparc.Core
             //var connectionString = Senparc.Scf.Core.Config.SenparcDatabaseConfigs.ClientConnectionString;
             //services.AddDbContext<SenparcEntities>(options => options.UseSqlServer(connectionString, b => b.MigrationsAssembly("Senparc.Web")));
 
+            /* 参考信息
+             *      错误信息：
+             *          中文：EnableRetryOnFailure 解决短暂的数据库连接失败
+             *          英文：Win32Exception: A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond
+             *                InvalidOperationException: An exception has been raised that is likely due to a transient failure. Consider enabling transient error resiliency by adding 'EnableRetryOnFailure()' to the 'UseSqlServer' call.
+             *      问题解决方案说明：https://www.colabug.com/2329124.html
+             */
+
             services.AddScoped(s => new SenparcEntities(new DbContextOptionsBuilder<SenparcEntities>()
                 .UseSqlServer(Scf.Core.Config.SenparcDatabaseConfigs.ClientConnectionString)
                 .Options));
