372be1123a26e827d170db2359589e5ac77015ca
苏震巍 <zsu@senparc.com>
2019-11-03 23:29:06 +0800

    根据新的SDK，优化

--- src/Senparc.Web/Startup.cs
+++ src/Senparc.Web/Startup.cs
@@ -71,9 +71,7 @@ namespace Senparc.Web
 
             services.AddSingleton<Core.Cache.RedisProvider.IRedisProvider, Core.Cache.RedisProvider.StackExchangeRedisProvider>();
 
-            services.AddSenparcGlobalServices(Configuration) //Senparc.CO2NET 全局注册
-                   .AddSenparcWeixinServices(Configuration); //Senparc.Weixin 注册
-
+            services.AddSenparcWeixinServices(Configuration); //Senparc.Weixin 注册（已自带 Senparc.CO2NET 全局注册）
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -96,15 +94,8 @@ namespace Senparc.Web
             app.UseStaticFiles();
             app.UseStaticFiles(new StaticFileOptions()
             {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),@"node_modules")),
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
                 RequestPath = new PathString("/node_modules")
-            });
-
-            app.Use(async (context, next) =>
-            {
-                var hubContext = context.RequestServices
-                                        .GetRequiredService<IHubContext<MyHub>>();
-                //...
             });
 
 
@@ -113,165 +104,166 @@ namespace Senparc.Web
 
             app.UseMvc();
 
-            #region CO2NET
-
             // 启动 CO2NET 全局注册，必须！
-            IRegisterService register = RegisterService.Start(env, senparcSetting.Value)
-                //关于 UseSenparcGlobal() 的更多用法见 CO2NET Demo：https://github.com/Senparc/Senparc.CO2NET/blob/master/Sample/Senparc.CO2NET.Sample.netcore/Startup.cs
-                .UseSenparcGlobal();
+            // 关于 UseSenparcGlobal() 的更多用法见 CO2NET Demo：https://github.com/Senparc/Senparc.CO2NET/blob/master/Sample/Senparc.CO2NET.Sample.netcore3/Startup.cs
+            var registerService = app
+                //全局注册
+                .UseSenparcGlobal(env, senparcSetting.Value, globalRegister =>
+                {
+                    #region CO2NET 全局配置
 
-            #region 全局缓存配置（按需）
+                    #region 全局缓存配置（按需）
 
-            //当同一个分布式缓存同时服务于多个网站（应用程序池）时，可以使用命名空间将其隔离（非必须）
-            register.ChangeDefaultCacheNamespace("SCFCache");
+                    #region 配置和使用 Redis
 
-            #region 配置和使用 Redis
-
-            //配置全局使用Redis缓存（按需，独立）
-            var redisConfigurationStr = senparcSetting.Value.Cache_Redis_Configuration;
-            var useRedis = !string.IsNullOrEmpty(redisConfigurationStr) && redisConfigurationStr != "Redis配置";
-            if (useRedis) //这里为了方便不同环境的开发者进行配置，做成了判断的方式，实际开发环境一般是确定的，这里的if条件可以忽略
-            {
-                /* 说明：
-                 * 1、Redis 的连接字符串信息会从 Config.SenparcSetting.Cache_Redis_Configuration 自动获取并注册，如不需要修改，下方方法可以忽略
-                /* 2、如需手动修改，可以通过下方 SetConfigurationOption 方法手动设置 Redis 链接信息（仅修改配置，不立即启用）
-                 */
-                Senparc.CO2NET.Cache.Redis.Register.SetConfigurationOption(redisConfigurationStr);
-
-                //以下会立即将全局缓存设置为 Redis
-                Senparc.CO2NET.Cache.Redis.Register.UseKeyValueRedisNow(); //键值对缓存策略（推荐）
-                //Senparc.CO2NET.Cache.Redis.Register.UseHashRedisNow();//HashSet储存格式的缓存策略
-
-                //也可以通过以下方式自定义当前需要启用的缓存策略
-                //CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisObjectCacheStrategy.Instance);//键值对
-                //CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisHashSetObjectCacheStrategy.Instance);//HashSet
-            }
-            //如果这里不进行Redis缓存启用，则目前还是默认使用内存缓存 
-
-            #endregion
-
-            #region 注册日志（按需，建议）
-
-            register.RegisterTraceLog(ConfigTraceLog); //配置TraceLog
-
-            #endregion
-
-            #endregion
-
-            #endregion
-
-            #region Weixin 设置
-
-            /* 微信配置开始
-             * 
-             * 建议按照以下顺序进行注册，尤其须将缓存放在第一位！
-             */
-
-            //注册开始
-
-            #region 微信缓存（按需，必须在 register.UseSenparcWeixin () 之前）
-
-            //微信的 Redis 缓存，如果不使用则注释掉（开启前必须保证配置有效，否则会抛错）
-            if (useRedis)
-            {
-                app.UseSenparcWeixinCacheRedis();
-            }
-
-            #endregion
-
-            //开始注册微信信息，必须！
-            register.UseSenparcWeixin(senparcWeixinSetting.Value, senparcSetting.Value)
-                //注意：上一行没有 ; 下面可接着写 .RegisterXX()
-            #region 注册公众号或小程序（按需）
-
-                //注册公众号（可注册多个）
-                .RegisterMpAccount(senparcWeixinSetting.Value, "SCF")
-                .RegisterMpAccount("", "", "Senparc_Template")
-
-                //注册多个公众号或小程序（可注册多个）
-                //.RegisterWxOpenAccount(senparcWeixinSetting.Value, "【盛派网络小助手】小程序")
-                //注册第三方平台（可注册多个）
-            #region 注册第三方平台
-
-                .RegisterOpenComponent(senparcWeixinSetting.Value,
-                  //getComponentVerifyTicketFunc
-                  async componentAppId =>
+                    //配置全局使用Redis缓存（按需，独立）
+                    if (UseRedis(senparcSetting.Value, out string redisConfigurationStr))//这里为了方便不同环境的开发者进行配置，做成了判断的方式，实际开发环境一般是确定的，这里的if条件可以忽略
                     {
+                        /* 说明：
+                         * 1、Redis 的连接字符串信息会从 Config.SenparcSetting.Cache_Redis_Configuration 自动获取并注册，如不需要修改，下方方法可以忽略
+                        /* 2、如需手动修改，可以通过下方 SetConfigurationOption 方法手动设置 Redis 链接信息（仅修改配置，不立即启用）
+                         */
+                        Senparc.CO2NET.Cache.Redis.Register.SetConfigurationOption(redisConfigurationStr);
 
-                        var dir = Path.Combine(ServerUtility.ContentRootMapPath("~/App_Data/OpenTicket"));
-                        if (!Directory.Exists(dir))
-                        {
-                            Directory.CreateDirectory(dir);
-                        }
+                        //以下会立即将全局缓存设置为 Redis
+                        Senparc.CO2NET.Cache.Redis.Register.UseKeyValueRedisNow(); //键值对缓存策略（推荐）
+                                                                                   //Senparc.CO2NET.Cache.Redis.Register.UseHashRedisNow();//HashSet储存格式的缓存策略
 
-                        var file = Path.Combine(dir, string.Format("{0}.txt", componentAppId));
-                        using (var fs = new FileStream(file, FileMode.Open))
-                        {
-                            using (var sr = new StreamReader(fs))
-                            {
-                                var ticket = await sr.ReadToEndAsync();
-                                return ticket;
-                            }
-                        }
-                    },
+                        //也可以通过以下方式自定义当前需要启用的缓存策略
+                        //CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisObjectCacheStrategy.Instance);//键值对
+                        //CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisHashSetObjectCacheStrategy.Instance);//HashSet
+                    }
+                    //如果这里不进行Redis缓存启用，则目前还是默认使用内存缓存 
 
-                   //getAuthorizerRefreshTokenFunc
-                   async (componentAppId, auhtorizerId) =>
-                     {
-                         var dir = Path.Combine(ServerUtility.ContentRootMapPath("~/App_Data/AuthorizerInfo/" + componentAppId));
-                         if (!Directory.Exists(dir))
-                         {
-                             Directory.CreateDirectory(dir);
-                         }
+                    #endregion
 
-                         var file = Path.Combine(dir, string.Format("{0}.bin", auhtorizerId));
-                         if (!System.IO.File.Exists(file))
-                         {
-                             return null;
-                         }
+                    #endregion
 
-                         using (Stream fs = new FileStream(file, FileMode.Open))
-                         {
-                             var binFormat = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-                             var result = (RefreshAuthorizerTokenResult)binFormat.Deserialize(fs);
-                             return result.authorizer_refresh_token;
-                         }
-                     },
+                    #region 注册日志（按需，建议）
 
-                     //authorizerTokenRefreshedFunc
-                     (componentAppId, auhtorizerId, refreshResult) =>
-                     {
-                         var dir = Path.Combine(ServerUtility.ContentRootMapPath("~/App_Data/AuthorizerInfo/" + componentAppId));
-                         if (!Directory.Exists(dir))
-                         {
-                             Directory.CreateDirectory(dir);
-                         }
+                    globalRegister.RegisterTraceLog(ConfigTraceLog); //配置TraceLog
 
-                         var file = Path.Combine(dir, string.Format("{0}.bin", auhtorizerId));
-                         using (Stream fs = new FileStream(file, FileMode.Create))
-                         {
-                             var binFormat = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-                             binFormat.Serialize(fs, refreshResult);
-                             fs.Flush();
-                         }
-                     }, "【盛派网络】开放平台")
+                    #endregion
 
-            #endregion
-                //除此以外，仍然可以在程序任意地方注册公众号或小程序：
-                //AccessTokenContainer.Register(appId, appSecret, name);//命名空间：Senparc.Weixin.MP.Containers
 
-            #endregion
+                    #endregion
+                })
+                //使用 Senparc.Weixin SDK
+                .UseSenparcWeixin(senparcWeixinSetting.Value, weixinRegister =>
+                {
+                    #region Weixin 设置
 
-            #region 注册微信支付（按需）
+                    /* 微信配置开始
+                     * 
+                     * 建议按照以下顺序进行注册，尤其须将缓存放在第一位！
+                     */
 
-                //注册最新微信支付版本（V3）（可注册多个）
-                .RegisterTenpayV3(senparcWeixinSetting.Value, "SCF") //记录到同一个 SenparcWeixinSettingItem 对象中
+                    //注册开始
 
-            #endregion
+                    #region 微信缓存（按需，必须在 register.UseSenparcWeixin () 之前）
 
-            ;
+                    //微信的 Redis 缓存，如果不使用则注释掉（开启前必须保证配置有效，否则会抛错）
+                    if (UseRedis(senparcSetting.Value, out string redisConfigurationStr))//这里为了方便不同环境的开发者进行配置，做成了判断的方式，实际开发环境一般是确定的，这里的if条件可以忽略
+                    {
+                        app.UseSenparcWeixinCacheRedis();
+                    }
 
-            #endregion
+                    #endregion
+
+                    #region 注册公众号或小程序（按需）
+
+                    //注册公众号（可注册多个）
+                    weixinRegister.RegisterMpAccount(senparcWeixinSetting.Value, "SCF")
+                                  .RegisterMpAccount("AppId", "Secret", "Senparc_Template")
+
+                        //注册多个公众号或小程序（可注册多个）
+                        //.RegisterWxOpenAccount(senparcWeixinSetting.Value, "【盛派网络小助手】小程序")
+                        //注册第三方平台（可注册多个）
+
+                    #region 注册第三方平台
+
+                        .RegisterOpenComponent(senparcWeixinSetting.Value,
+                          //getComponentVerifyTicketFunc
+                          async componentAppId =>
+                          {
+
+                              var dir = Path.Combine(ServerUtility.ContentRootMapPath("~/App_Data/OpenTicket"));
+                              if (!Directory.Exists(dir))
+                              {
+                                  Directory.CreateDirectory(dir);
+                              }
+
+                              var file = Path.Combine(dir, string.Format("{0}.txt", componentAppId));
+                              using (var fs = new FileStream(file, FileMode.Open))
+                              {
+                                  using (var sr = new StreamReader(fs))
+                                  {
+                                      var ticket = await sr.ReadToEndAsync();
+                                      return ticket;
+                                  }
+                              }
+                          },
+
+                           //getAuthorizerRefreshTokenFunc
+                           async (componentAppId, auhtorizerId) =>
+                           {
+                               var dir = Path.Combine(ServerUtility.ContentRootMapPath("~/App_Data/AuthorizerInfo/" + componentAppId));
+                               if (!Directory.Exists(dir))
+                               {
+                                   Directory.CreateDirectory(dir);
+                               }
+
+                               var file = Path.Combine(dir, string.Format("{0}.bin", auhtorizerId));
+                               if (!System.IO.File.Exists(file))
+                               {
+                                   return null;
+                               }
+
+                               using (Stream fs = new FileStream(file, FileMode.Open))
+                               {
+                                   var binFormat = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                                   var result = (RefreshAuthorizerTokenResult)binFormat.Deserialize(fs);
+                                   return result.authorizer_refresh_token;
+                               }
+                           },
+
+                             //authorizerTokenRefreshedFunc
+                             (componentAppId, auhtorizerId, refreshResult) =>
+                             {
+                                 var dir = Path.Combine(ServerUtility.ContentRootMapPath("~/App_Data/AuthorizerInfo/" + componentAppId));
+                                 if (!Directory.Exists(dir))
+                                 {
+                                     Directory.CreateDirectory(dir);
+                                 }
+
+                                 var file = Path.Combine(dir, string.Format("{0}.bin", auhtorizerId));
+                                 using (Stream fs = new FileStream(file, FileMode.Create))
+                                 {
+                                     var binFormat = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                                     binFormat.Serialize(fs, refreshResult);
+                                     fs.Flush();
+                                 }
+                             }, "【盛派网络】开放平台")
+
+                    #endregion
+
+                        //除此以外，仍然可以在程序任意地方注册公众号或小程序：
+                        //AccessTokenContainer.Register(appId, appSecret, name);//命名空间：Senparc.Weixin.MP.Containers
+
+                    #endregion
+
+                    #region 注册微信支付（按需）
+
+                        //注册最新微信支付版本（V3）（可注册多个）
+                        .RegisterTenpayV3(senparcWeixinSetting.Value, "SCF") //记录到同一个 SenparcWeixinSettingItem 对象中
+
+                    #endregion
+
+                    ;
+
+                    #endregion
+
+                });
 
             #region .NET Core默认不支持GB2312
 
@@ -348,8 +340,19 @@ namespace Senparc.Web
                 //eventService.ConfigOnWeixinExceptionFunc(ex);
             };
         }
-    }
 
+        /// <summary>
+        /// 判断当前配置是否满足使用 Redis（根据是否已经修改了默认配置字符串判断）
+        /// </summary>
+        /// <param name="senparcSetting"></param>
+        /// <returns></returns>
+        private bool UseRedis(SenparcSetting senparcSetting, out string redisConfigurationStr)
+        {
+            redisConfigurationStr = senparcSetting.Cache_Redis_Configuration;
+            var useRedis = !string.IsNullOrEmpty(redisConfigurationStr) && redisConfigurationStr != "#{Cache_Redis_Configuration}#"/*默认值，不启用*/;
+            return useRedis;
+        }
+    }
 
 
     public static class PhysicalFileAppBuilderExtensions
