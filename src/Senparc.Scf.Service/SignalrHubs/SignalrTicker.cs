934da0a0909217521c48dce35a1e784dce6b1356
苏震巍 <zsu@senparc.com>
2019-11-03 22:07:20 +0800

    添加 SignalrTicker

---
+++ src/Senparc.Scf.Service/SignalrHubs/SignalrTicker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.Scf.Service.SignalrHubs
+{
+    /// <summary>
+    /// 记录 SignalR 全局上下文的类，可轮询执行操作
+    /// </summary>
+    public class SignalrTicker
+    {
+
+
+    }
+}
