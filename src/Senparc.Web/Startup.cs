4903f3357bf0aeb58b02b5e727458fb6441dc344
苏震巍 <zsu@senparc.com>
2019-11-06 22:29:53 +0800

    modify

--- src/Senparc.Web/Startup.cs
+++ src/Senparc.Web/Startup.cs
@@ -86,7 +86,7 @@ namespace Senparc.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseStaticFiles(new StaticFileOptions()
