5ff75573c92f4cae58e7073ddee6a36af9067834
苏震巍 <zsu@senparc.com>
2020-02-04 18:35:04 +0800

    提交新的菜单变更：add-migration Update-SysMenuDto

--- src/Senparc.Web/Migrations/20200204103009_Update-SysMenuDto.Designer.cs
+++ src/Senparc.Web/Migrations/20200204103009_Update-SysMenuDto.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Senparc.Core.Models;
 
 namespace Senparc.Web.Migrations
 {
     [DbContext(typeof(SenparcEntities))]
-    partial class SenparcEntitiesModelSnapshot : ModelSnapshot
+    [Migration("20200204103009_Update-SysMenuDto")]
+    partial class UpdateSysMenuDto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Senparc.Web/Migrations/20200204103009_Update-SysMenuDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Senparc.Web.Migrations
+{
+    public partial class UpdateSysMenuDto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResourceCode",
+                table: "SysPermission",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResourceCode",
+                table: "SysPermission");
+        }
+    }
+}
