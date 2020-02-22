e922437ab9da2bde54d00028056888b24cd3f3b8
JeffreySu <www.jeffrey.su@gmail.com>
2020-02-22 19:53:12 +0800

    优化模块详情页

--- src/Senparc.Areas.Admin/Areas/Admin/Pages/XscfModule/Start.cshtml.cs
+++ src/Senparc.Areas.Admin/Areas/Admin/Pages/XscfModule/Start.cshtml.cs
@@ -1,19 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Senparc.CO2NET.Extensions;
 using Senparc.Scf.Core.Enums;
 using Senparc.Scf.Service;
 using Senparc.Scf.XscfBase;
+using Senparc.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Senparc.Areas.Admin.Areas.Admin.Pages
 {
     public class XscfModuleStartModel : BaseAdminPageModel
     {
         public IXscfRegister XscfRegister { get; set; }
+        private readonly SysMenuService _sysMenuService;
         public Senparc.Scf.Core.Models.DataBaseModel.XscfModule XscfModule { get; set; }
         public Dictionary<IXscfFunction, List<FunctionParammeterInfo>> FunctionParammeterInfoCollection { get; set; } = new Dictionary<IXscfFunction, List<FunctionParammeterInfo>>();
 
@@ -23,10 +24,11 @@ namespace Senparc.Areas.Admin.Areas.Admin.Pages
         public string Msg { get; set; }
         public object Obj { get; set; }
 
-        public XscfModuleStartModel(IServiceProvider serviceProvider, XscfModuleService xscfModuleService)
+        public XscfModuleStartModel(IServiceProvider serviceProvider, XscfModuleService xscfModuleService, SysMenuService sysMenuService)
         {
             _serviceProvider = serviceProvider;
             _xscfModuleService = xscfModuleService;
+            _sysMenuService = sysMenuService;
         }
 
         public async Task OnGetAsync(string uid)
@@ -78,5 +80,41 @@ namespace Senparc.Areas.Admin.Areas.Admin.Pages
             return RedirectToPage("Start", new { uid = module.Uid });
         }
 
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var module = await _xscfModuleService.GetObjectAsync(z => z.Id == id).ConfigureAwait(false);
+
+            if (module == null)
+            {
+                throw new Exception("模块未添加！");
+            }
+
+            //删除菜单
+            Func<Task> uninstall = async () =>
+            {
+                //删除菜单
+                var menu = await _sysMenuService.GetObjectAsync(z => z.MenuName == module.MenuName).ConfigureAwait(false);
+                if (menu != null)
+                {
+                    await _sysMenuService.DeleteObjectAsync(menu).ConfigureAwait(false);
+                }
+                await _xscfModuleService.DeleteObjectAsync(module).ConfigureAwait(false);
+            };
+
+
+            //尝试从已加载的模块中执行删除过程
+            var register = Senparc.Scf.XscfBase.Register.RegisterList.FirstOrDefault(z => z.Uid == module.Uid);
+            if (register == null)
+            {
+                //直接删除，如dll已经不存在，可能引发此问题，只能在当前系统内直接执行删除
+                await uninstall().ConfigureAwait(false);
+            }
+            else
+            {
+                await register.UninstallAsync(uninstall).ConfigureAwait(false);
+            }
+
+            return RedirectToPage("Index");
+        }
     }
 }
