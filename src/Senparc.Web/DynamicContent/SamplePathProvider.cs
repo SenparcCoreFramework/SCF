147ee18b55a98e81736ad674f73d0385bfe844e0
JeffreySu <www.jeffrey.su@gmail.com>
2020-03-02 17:24:35 +0800

    添加 SamplePathProvider

---
+++ src/Senparc.Web/DynamicContent/SamplePathProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senparc.Web.DynamicContent
+{
+    public class SamplePathProvider
+    {
+    }
+}
