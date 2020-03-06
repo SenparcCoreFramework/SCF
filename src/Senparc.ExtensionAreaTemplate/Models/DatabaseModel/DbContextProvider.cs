ff3f1b701683e29e3c59b6b0c19a72d1c0cc76b2
JeffreySu <www.jeffrey.su@gmail.com>
2020-03-06 21:58:11 +0800

    modify

---
+++ src/Senparc.ExtensionAreaTemplate/Models/DatabaseModel/DbContextProvider.cs
@@ -0,0 +1,34 @@
+﻿using Senparc.Scf.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Senparc.ExtensionAreaTemplate.Models.DatabaseModel
+{
+    /// <summary>
+    /// IDbContextProvider
+    /// </summary>
+    public interface IDbContextProvider
+    {
+        SenparcEntitiesBase Get();
+    }
+
+
+    public class DbContextProvider : IDbContextProvider
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DbContextProvider(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public SenparcEntitiesBase Get()
+        {
+            var senparcEntities =  _serviceProvider.GetService<SenparcEntitiesBase>();
+            //senparcEntities.
+                return senparcEntities;
+        }
+    }
+}
