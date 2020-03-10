ad48db4c1aaae4cc407ed87e2dd682f59f11a34c
JeffreySu <www.jeffrey.su@gmail.com>
2020-03-10 21:29:25 +0800

    更新管理后台和注册代码

--- src/Senparc.Web/Register.cs
+++ src/Senparc.Web/Register.cs
@@ -15,6 +15,7 @@ using Senparc.Scf.Core.Areas;
 using Senparc.Scf.Core.AssembleScan;
 using Senparc.Scf.Core.Config;
 using Senparc.Scf.Core.Models;
+using Senparc.Scf.Service;
 using Senparc.Scf.SMS;
 using Senparc.Scf.XscfBase;
 using System;
@@ -152,6 +153,8 @@ namespace Senparc.Web
 
             //Repository
             services.AddScoped(typeof(Senparc.Scf.Repository.IRepositoryBase<>), typeof(Senparc.Scf.Repository.RepositoryBase<>));
+            services.AddScoped(typeof(ServiceBase<>));
+            services.AddScoped(typeof(IServiceBase<>), typeof(ServiceBase<>));
             services.AddScoped(typeof(ISysButtonRespository), typeof(SysButtonRespository));
             //Other
             services.AddScoped(typeof(Scf.Core.WorkContext.Provider.IAdminWorkContextProvider), typeof(Scf.Core.WorkContext.Provider.AdminWorkContextProvider));
