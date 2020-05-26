f146f640ea27233841f353a15e6cf0483439cfe9
JeffreySu <www.jeffrey.su@gmail.com>
2020-05-27 00:46:23 +0800

    迁移文件

--- src/Senparc.Web/SenparcBaseDbContextFactory.cs
+++ src/Senparc.Web/SenparcBaseDbContextFactory.cs
@@ -1,23 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.DependencyInjection;
 using Senparc.CO2NET;
-using Senparc.CO2NET.Extensions;
 using Senparc.CO2NET.RegisterServices;
-using Senparc.CO2NET.Utilities;
 using Senparc.Core.Models;
 using Senparc.Scf.Core.Config;
-using Senparc.Scf.Core.Models;
 using System;
-using System.Diagnostics;
 using System.IO;
 
 namespace Senparc.Web
 {
     /// <summary>
-    /// 设计时 DbContext 创建
+    /// 设计时 DbContext 创建，用于更新底层（Senparc.Scf.Core）的实体，仅供开发时使用
     /// </summary>
-    public class SenparcDbContextFactory : IDesignTimeDbContextFactory<SenparcEntities>
+    public class SenparcBaseDbContextFactory : IDesignTimeDbContextFactory<SenparcEntities>
     {
         public SenparcEntities CreateDbContext(string[] args)
         {
@@ -28,21 +23,22 @@ namespace Senparc.Web
 
             CO2NET.Config.RootDictionaryPath = Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\"); //
 
-            var builder = new DbContextOptionsBuilder<SenparcEntities>();
 
             //如果运行 Add-Migration 命令，并且获取不到正确的网站根目录，此处可能无法自动获取到连接字符串（上述#13问题），
             //也可通过下面已经注释的的提供默认值方式解决（不推荐）
             var sqlConnection = SenparcDatabaseConfigs.ClientConnectionString; //?? "Server=.\\;Database=SCF;Trusted_Connection=True;integrated security=True;";
+
             Console.WriteLine("============================");
             Console.WriteLine(SenparcDatabaseConfigs.ClientConnectionString);
             Console.WriteLine("============================");
 
             Service.Register systemServiceRegister = new Service.Register();
-            
+
             Senparc.Scf.XscfBase.Register.XscfDatabaseList.Add(systemServiceRegister);//添加注册
 
-            //builder.UseSqlServer(sqlConnection, b => systemServiceRegister.DbContextOptionsAction(b, "Senparc.Web"));//beta4
-            builder.UseSqlServer(sqlConnection, b => systemServiceRegister.DbContextOptionsAction(b, "Senparc.Service"));//beta5
+            var builder = new DbContextOptionsBuilder<SenparcEntities>();
+
+            builder.UseSqlServer(sqlConnection, b => systemServiceRegister.DbContextOptionsAction(b, "Senparc.Web"));//beta5
             return new SenparcEntities(builder.Options);
         }
     }
