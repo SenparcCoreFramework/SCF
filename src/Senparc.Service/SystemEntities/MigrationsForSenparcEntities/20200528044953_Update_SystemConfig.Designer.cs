e463a7ac0684c84365c07ba94c3948ec6450305c
JeffreySu <www.jeffrey.su@gmail.com>
2020-05-28 12:56:24 +0800

    完成 HideModuleManager 属性添加

--- src/Senparc.Service/SystemEntities/MigrationsForSenparcEntities/20200528044953_Update_SystemConfig.Designer.cs
+++ src/Senparc.Service/SystemEntities/MigrationsForSenparcEntities/20200528044953_Update_SystemConfig.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Senparc.Core.Models;
 
 namespace Senparc.Service.SystemEntities.MigrationsForSenparcEntities
 {
     [DbContext(typeof(SenparcEntities))]
-    partial class SenparcEntitiesModelSnapshot : ModelSnapshot
+    [Migration("20200528044953_Update_SystemConfig")]
+    partial class Update_SystemConfig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
