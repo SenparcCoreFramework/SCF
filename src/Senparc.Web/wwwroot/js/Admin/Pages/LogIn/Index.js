2d6e6407bca0ee8fa4da06d5946df31fff959e18
时启龙 <472252510@qq.com>
2020-06-17 11:36:30 +0800

    引入element和vue，更改login页面

---
+++ src/Senparc.Web/wwwroot/js/Admin/Pages/LogIn/Index.js
@@ -0,0 +1,46 @@
+﻿var validatePass = (rule, value, callback) => {
+    if (value === '') {
+        callback(new Error('请输入密码'));
+    } else {
+        callback();
+    }
+};
+var validateUser = (rule, value, callback) => {
+    if (value === '') {
+        callback(new Error('请输入用户名'));
+    } else {
+        callback();
+    }
+};
+var app = new Vue({
+    el: '#app',
+    data: {
+        ruleForm: {
+            user: '',
+            checkPass: ''
+        },
+        rules: {
+            user: [
+                { validator: validateUser, trigger: 'blur' }
+            ],
+            pass: [
+                { validator: validatePass, trigger: 'blur' }
+            ]
+        }
+    },
+    mounted() {
+        console.log(1);
+    },
+    methods: {
+        submitForm(formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    alert('submit!');
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
+        }
+    }
+});
