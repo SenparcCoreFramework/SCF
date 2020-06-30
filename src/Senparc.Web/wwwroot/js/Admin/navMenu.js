e7581f20b3ca04500242f300b37980c5fe8b487d
时启龙 <472252510@qq.com>
2020-06-30 19:38:41 +0800

    优化菜单栏

--- src/Senparc.Web/wwwroot/js/Admin/navMenu.js
+++ src/Senparc.Web/wwwroot/js/Admin/navMenu.js
@@ -1,4 +1,4 @@
-﻿// 菜单栏数据
+﻿//// 菜单栏数据
 const navMenu = {
     //侧边栏
     navMenuList: [],
@@ -9,35 +9,43 @@ const navMenu = {
         menuActiveText: '#409EFF' //激活颜色
     },
     // 当前激活菜单的 index
-    activeMenu: window.sessionStorage.getItem('activeMenu') || '0',
-    loginout: function loginout() {
-        app.$confirm('是否退出登录？', { type: 'warning' }).then(res => {
-            window.sessionStorage.removeItem('activeMenu');
-            window.location.href = '/Admin/Login?handler=Logout';
-        }).catch(e => {
-            //
-        });
-    }
+    activeMenu: window.sessionStorage.getItem('activeMenu') || '0'
 };
+// 菜单栏数据
+//Vue.prototype.navMenu = {
+//    //侧边栏
+//    navMenuList: [],
+//    isCollapse: false,
+//    variables: {
+//        menuBg: '#304156', // 背景色
+//        menuText: '#bfcbd9', // 文字色
+//        menuActiveText: '#409EFF' //激活颜色
+//    },
+//    // 当前激活菜单的 index
+//    activeMenu: window.sessionStorage.getItem('activeMenu') || '0'
+//};
+// 切换菜单栏展开
+Vue.prototype.toggleSideBar = function () {
+    app.navMenu.isCollapse = !app.navMenu.isCollapse;
+    console.log(app.navMenu.isCollapse);
+};
+
+Vue.prototype.menuSelect = function (key, keypath) {
+    window.sessionStorage.setItem('activeMenu', key);
+};
+
 // 菜单栏数据
 (function getNavMenu() {
     service.get("/Admin/index?handler=MenuResource").then(res => {
         if (res.data.success) {
             var ddd = res.data.data.menuList;
             myfunctionMain(ddd);
-            navMenu.navMenuList = ddd;
+            app.navMenu.navMenuList = ddd;
             // 按钮权限存起来  使用：直接在dom上v-has=" ['admin-add']"
             window.sessionStorage.setItem('saveResourceCodes', JSON.stringify(res.data.data.resourceCodes));
         }
     });
 })();
-function menuSelect(key, keyPath) {
-    window.sessionStorage.setItem('activeMenu', key);
-}
-// 切换菜单栏展开
-function toggleSideBar() {
-    navMenu.isCollapse = !navMenu.isCollapse;
-}
 
 // 菜单栏数据递归
 function myfunctionMain(list) {
