ddddb6b5e0ee563565f26f52cb7b89dc3812136f
时启龙 <472252510@qq.com>
2020-07-02 14:31:42 +0800

    删除js文件，修改Startup.cs，签入chart、echart

--- src/Senparc.Web/Startup.cs
+++ src/Senparc.Web/Startup.cs
@@ -56,11 +56,6 @@ namespace Senparc.Web
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseStaticFiles(new StaticFileOptions()
-            {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
-                RequestPath = new PathString("/node_modules")
-            });
 
             app.UseCookiePolicy();
 
--- src/Senparc.Web/wwwroot/js/Admin/global.js
+++ src/Senparc.Web/wwwroot/js/Admin/global.js
@@ -7,158 +7,6 @@ Vue.prototype.loginout = function () {
 function formaTableTime(value) {
     return value ? value.replace('T', '  ').substr(0, 17) : '暂无时间';
 }
-/**
- * 递归，获取所有子节点
- * @param {any} treeNode 2
- * @param {any} result 2
- * @returns {any} 节点名称
- */
-function getAllChildrenNodes(treeNode, result) {
-    if (treeNode.isParent) {
-        var childrenNodes = treeNode.children;
-        if (childrenNodes) {
-            for (var i = 0; i < childrenNodes.length; i++) {
-                result.push(childrenNodes[i]);
-                result = getAllChildrenNodes(childrenNodes[i], result);
-            }
-        }
-    }
-    return result;
-}
-
-/**
- * 
- * @param {any} selector 选择器
- * @param {any} options 树选项
- * @returns {any} 树示例
- */
-async function initMenu(selector, options) {
-    return await _initMenuTree(selector || '#tree', '/Admin/Menu/Edit', { handler: 'menu' }, options);
-}
-
-/**
- * 初始化树
- * @param {any} selector 元素选择器
- * @param {any} url 请求地址
- * @param {any} params 请求参数
- * @param {any} options 选项
- * @returns {any} 树示例
- */
-async function _initMenuTree(selector, url, params, options) {
-    var setting = {
-        data: {
-            simpleData: {
-                enable: true,
-                pIdKey: "parentId",
-                rootPId: null
-            },
-            key: {
-                title: 'id',
-                name: 'menuName',
-                url: 'xdasdf'
-            }
-        }
-    };
-    setting = $.extend(setting, options);
-    let responseData = await base.get(url, params);
-    if (options.hiddenButton) {
-        responseData = responseData.filter((ele, index) => { return ele.isMenu; });
-    }
-    let treeObj = $.fn.zTree.init($(selector), setting, responseData);
-    if (options.IsExpendAll) {
-        treeObj.expandAll(true);
-    }
-    return treeObj;
-}
-
-/**
- * 
- * @param {any} selector 选择器
- * @param {any} options 树选项
- * @returns {any} 树示例
- */
-async function initCatalog(selector, options) {
-    return await _initCatalogTree(selector || '#tree', '/Admin/Document/Edit', { handler: 'catalog' }, options);
-}
-
-/**
- * 初始化树
- * @param {any} selector 元素选择器
- * @param {any} url 请求地址
- * @param {any} params 请求参数
- * @param {any} options 选项
- * @returns {any} 树示例
- */
-async function _initCatalogTree(selector, url, params, options) {
-    var setting = {
-        data: {
-            simpleData: {
-                enable: true,
-                pIdKey: "parentId",
-                rootPId: 0
-            },
-            key: {
-                title: 'id',
-                name: 'name',
-                url: 'xdasdf'
-            }
-        }
-    };
-    setting = $.extend(setting, options);
-    let responseData = await base.get(url, params);
-    if (options.hiddenButton) {
-        responseData = responseData.filter((ele, index) => { return ele.isMenu; });
-    }
-    debugger
-    let treeObj = $.fn.zTree.init($(selector), setting, responseData);
-    if (options.IsExpendAll) {
-        treeObj.expandAll(true);
-    }
-    return treeObj;
-}
-
-
-/**
- * 多选删除
- * @param {string} checkBoxName  checkbox 的name
- * @param {string} formId  表单的Id
- * @returns {any} 无返回值
- */
-function deleteCheck(checkBoxName, formId) {
-    var _checkedAll = $("input[name=" + checkBoxName + "]:checked");
-    //debugger
-    if (_checkedAll.length === 0) {
-        //base.("warning", "请选择需要删除的内容");
-        base.swal.alert('请选择需要删除的内容');
-        return false;
-    }
-
-    swal({
-        text: '确定要删除吗？',
-        icon: 'warning',
-        buttons: {
-            cancel: {
-                text: "取消",
-                value: null,
-                visible: true,
-                className: "",
-                closeModal: true,
-            },
-            confirm: {
-                text: "确定",
-                value: true,
-                visible: true,
-                className: "",
-                closeModal: true
-            }
-        }
-    }).then(v => {
-        if (v) {
-            $('#' + formId).submit();
-        }
-    });
-}
-
 
 /**
  * 复制内容
@@ -177,8 +25,8 @@ function copyToClipboard(value, toastText) {
     }
 }
 
-
-function resizeUrl() {//处理剪切url id
+// 处理剪切url id
+function resizeUrl() {
     let url = window.location.href;
     let obj = {};
     let reg = /[?&][^?&]+=[^?&]+/g;
@@ -203,14 +51,13 @@ function formatCurrency(num) {
     num = num.toString().replace(/\$|\,/g, '');
     if (isNaN(num))
         num = "0";
-    sign = num == (num = Math.abs(num));
+    sign = num === (num = Math.abs(num));
     num = Math.floor(num * 100 + 0.50000000001);
     cents = num % 100;
     num = Math.floor(num / 100).toString();
     if (cents < 10)
         cents = "0" + cents;
     for (var i = 0; i < Math.floor((num.length - (1 + i)) / 3); i++)
-        num = num.substring(0, num.length - (4 * i + 3)) + ',' +
-            num.substring(num.length - (4 * i + 3));
-    return (((sign) ? '' : '-') + num + '.' + cents);
+        num = num.substring(0, num.length - (4 * i + 3)) + ',' + num.substring(num.length - (4 * i + 3));
+    return ((sign ? '' : '-') + num + '.' + cents);
 }
